97497c6f998f7bd990c9d1fef3b919f625336256
Leonel Cruz <lyonel_cc@hotmail.com>
2018-10-18 09:45:54 -0500

    Refactory ModuleIR.py

--- System/Module_IR.py
+++ System/Module_IR.py
@@ -5,20 +5,12 @@ from pprint import pprint
 
 
 
-
 #Read the json file obtained from the crawler script.  
 with open('intro/lavanguardia.json') as f:
     data = json.load(f)
 
-#pprint(data)
 
-
-#subprocess.call(['/bin/ls', 'intro'])
-#subprocess.call("/usr/bin/ch_pdf_png.sh")
-#subprocess.call("/usr/bin/ch_pdf_png.sh")
-
-#Recorrer el json iterativamente y descargar los pdf en el directorio downloadPDF
-
+#Go through the jason file iteratively and download the pdfs in the downloadPDF directory
 for song in data:
 	for key, value in song.items():
 		if key=='PDF_URL':
@@ -28,21 +20,9 @@ for song in data:
 				filename = wget.download(subtring, out="downloadPDF")
 				print(subtring)
 
-#Transformar pdfs a png 
+#Transform the pdf to png using the package ImageMagic with the convert option. Also the
+#module subprocess is used to execute the script in bash. 
 subprocess.call("/usr/bin/ch_pdf_png.sh")
 
-#Eliminar los pdfs descargados.
+#Delete files pdfs inside  downloadPDF  directory after of conversion done.  
 subprocess.call("/usr/bin/del_pdfs.sh")
-
-#list = data[0]['PDF_URL']
-
-#for subtring in list:
-#	print(subtring)
-
-
-#subtring = subtring.replace("[","]","")
-#print("Este dato es:", subtring)
-
-
-#Download PDF
-#filename = wget.download(subtring, out="downloadPDF")
